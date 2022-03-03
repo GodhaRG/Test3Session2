e9bb8aa10dc5c5533f0513e1489f3e2a0376b195
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-03-03 09:36:26 +0000

    done 1 and 7

--- p1final.c
+++ p1final.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+void input(int *num1,int *den1,int *num2,int*den2)
+{
+  printf("enter one fraction\n");
+  scanf("%d %d",num1,den1);
+  printf("enter the other fraction\n");
+  scanf("%d %d",num2,den2);
+}
+int gcd(int den1,int den2)
+{
+  int t;
+  while(den2!=0)
+    {
+      t=den2;
+      den2=den1%den2;
+      den1=t;
+    }
+  return den1;
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+*num3=num1*den2+num2*den1;
+  *den3=den1*den2;
+  int g= gcd(*num3,*den3);
+  *num3=*num3/g;
+  *den3=*den3/g;
+  }
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf(" %d/%d + %d/%d= %d/%d\n",num1,den1,num2,den2,num3,den3 );
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
