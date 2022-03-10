41297ac73cd3d647fe63b411e3e9a43b9e23dceb
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-03-10 09:45:52 +0000

    done

--- p1final.c
+++ p1final.c
@@ -21,7 +21,7 @@ void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
 *num3=num1*den2+num2*den1;
   *den3=den1*den2;
-  int g= gcd(*num3,*den3);
+  int g = gcd(*num3,*den3);
   *num3=*num3/g;
   *den3=*den3/g;
   }
