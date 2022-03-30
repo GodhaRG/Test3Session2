7fb3b6cd1bd156329ecaa709514cd17fd1ec5340
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-03-30 15:22:43 +0000

    done 2

--- p2final.c
+++ p2final.c
@@ -1,24 +1,44 @@
 #include<stdio.h>
 struct _fraction
 {
-int num,den;
+float num,den;
 };
 typedef struct  _fraction Fraction;
 Fraction input_fraction()
 {
   Fraction f;
   printf("input a fraction\n");
-  scanf("%d%d",&f.num,&f.den);
+  scanf("%f%f",&f.num,&f.den);
   return f;
 }
 Fraction Largest_fraction(Fraction f1,Fraction f2,Fraction f3)
 {
-  
-  
-}
+  float s1,s2,s3;
+  Fraction largest;
+  s1=f1.num/f1.den;
+  s2=f2.num/f2.den;
+  s3=f3.num/f3.den;
+  if(s1<s2&&s1<s3)
+  {
+    largest.num=f1.num;
+    largest.den=f1.den;
+  }
+  else if(s2<s3&&s2<s1)
+  {
+    largest.num=f2.num;
+    largest.den=f2.den;
+  }
+  else
+  {
+    largest.num=f3.num;
+    largest.den=f3.den;
+  }
+  return largest;
+} 
+ 
 void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
 {
-  printf("the smallest of the three fractions is   %d / %d\n", largest.num,largest.den);
+  printf("the smallest of the three fractions is   %f / %f\n", largest.num,largest.den);
 }
 int main()
 {
--- p3final.c
+++ p3final.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 int input_n_and_r(int *n, int *r)
 {
